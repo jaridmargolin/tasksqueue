fc9a39dd862489a0f9bb7e6b2a0afc30b60147d2
Jarid Margolin <jaridmargolin@gmail.com>
2017-06-23 19:08:51 -0400

    Implement 1.0.0 rewrite.

---
+++ README.md
@@ -0,0 +1,27 @@
+<h1>task-queue.js</h1>
+<div>
+  <p>Extensible async task queue.</p>
+  <div>
+  <a href="https://travis-ci.org/jaridmargolin/task-queue.js"><img src="https://travis-ci.org/jaridmargolin/task-queue.js.svg?branch=master" alt="Build Status"></a>
+  <a href="https://coveralls.io/github/jaridmargolin/task-queue.js?branch=master"><img src="https://coveralls.io/repos/github/jaridmargolin/task-queue.js/badge.svg?branch=master" alt="Coverage Status"></a>
+  <a href="http://standardjs.com/"><img src="https://img.shields.io/badge/code%20style-standard-brightgreen.svg" alt="Standard - JavaScript Style Guide"></a>
+  </div>
+  <div>
+  <a href="https://npmjs.org/package/task-queue.js"><img src="https://img.shields.io/npm/v/task-queue.js.svg" alt="NPM task-queue.js package"></a>
+  <a href="https://david-dm.org/jaridmargolin/task-queue.js"><img src="https://david-dm.org/jaridmargolin/task-queue.js.svg" alt="Dependency Status"></a>
+  <a href="https://david-dm.org/jaridmargolin/task-queue.js#info=devDependencies"><img src="https://david-dm.org/jaridmargolin/task-queue.js/dev-status.svg" alt="devDependency Status"></a>
+  </div>
+  <a href="http://jaridmargolin.github.io/task-queue.js">View Documentation</a>
+</div>
+<br>
+
+
+## License
+
+The MIT License (MIT) Copyright (c) Jarid Margolin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ test/task-queue_test.js
@@ -0,0 +1,124 @@
+/* eslint-env mocha */
+'use strict'
+
+/* -----------------------------------------------------------------------------
+ * dependencies
+ * -------------------------------------------------------------------------- */
+
+import { assert } from 'chai'
+import sinon from 'sinon'
+import TaskQueue from '../src'
+
+/* -----------------------------------------------------------------------------
+ * test
+ * -------------------------------------------------------------------------- */
+
+describe('task-queue.js', function () {
+  beforeEach(function () {
+    this.processed = []
+
+    this.queue = new TaskQueue(function (task, next) {
+      this.processed.push(task.val)
+      setTimeout(next, 0)
+    }.bind(this))
+  })
+
+  it('Should add task to head of queue.', function () {
+    var task = { val: 1 }
+    this.queue.add(task)
+
+    assert.equal(this.queue.tasks[0], task)
+  })
+
+  it('Should return true if queue is empty.', function () {
+    assert.isTrue(this.queue.isEmpty())
+  })
+
+  it('Should return false if queue contains tasks.', function () {
+    this.queue.add({ val: 1 })
+
+    assert.isFalse(this.queue.isEmpty())
+  })
+
+  it('Should immediately process from head of queue.', function (done) {
+    this.queue.add({ val: 1 }, true)
+
+    setTimeout(function () {
+      assert.isTrue(this.queue.isEmpty())
+      assert.deepEqual(this.processed, [1])
+      done()
+    }.bind(this), 100)
+  })
+
+  it('Should add to queue and process after current executing task.', function (done) {
+    this.queue.add({ val: 1 }, true)
+    this.queue.add({ val: 2 })
+
+    setTimeout(function () {
+      assert.isTrue(this.queue.isEmpty())
+      assert.deepEqual(this.processed, [1, 2])
+      done()
+    }.bind(this), 100)
+  })
+
+  it('Should add multiple tasks to queue.', function (done) {
+    this.queue.add([
+      { val: 1 },
+      { val: 2 }
+    ], true)
+
+    setTimeout(function () {
+      assert.isTrue(this.queue.isEmpty())
+      assert.deepEqual(this.processed, [1, 2])
+      done()
+    }.bind(this), 100)
+  })
+
+  it('Should not add duplicate entires to list.', function (done) {
+    this.queue.add([
+      { id: 1, val: 1 },
+      { id: 1, val: 2 }
+    ], true)
+
+    setTimeout(function () {
+      assert.isTrue(this.queue.isEmpty())
+      assert.deepEqual(this.processed, [1])
+      assert.deepEqual(this.queue.indexes, {})
+      done()
+    }.bind(this), 100)
+  })
+
+  it('Should stop processing after last task.', function (done) {
+    this.queue.add({ val: 1 }, true)
+
+    setTimeout(function () {
+      this.queue.add({ val: 2 })
+    }.bind(this), 100)
+
+    setTimeout(function () {
+      assert.isFalse(this.queue.isEmpty())
+      assert.deepEqual(this.processed, [1])
+      done()
+    }.bind(this), 100)
+  })
+
+  it('Should clear queue and indexes.', function () {
+    this.queue.add([
+      { id: 1, val: 1 },
+      { id: 2, val: 2 }
+    ])
+
+    this.queue.clear()
+
+    assert.isTrue(this.queue.isEmpty())
+    assert.deepEqual(this.queue.tasks, [])
+    assert.deepEqual(this.queue.indexes, {})
+  })
+
+  it('Should clear on process when flag set.', function () {
+    this.queue.shiftOnProcess = true
+    this.queue.add({ val: 1 }, true)
+
+    assert.deepEqual(this.queue.tasks, [])
+  })
+})
