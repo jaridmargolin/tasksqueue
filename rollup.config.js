d0697a35124f1978efb222ebb436eec4738395bb
Jarid Margolin <jaridmargolin@gmail.com>
2019-12-21 23:28:54 -0800

    Update build process to utilize core-js

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,9 @@
  * dependencies
  * -------------------------------------------------------------------------- */
 
+// 3rd party
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
 import babel from 'rollup-plugin-babel'
 
@@ -11,7 +14,17 @@ import babel from 'rollup-plugin-babel'
  * rollup config
  * -------------------------------------------------------------------------- */
 
+const externals = [
+  'core-js/modules/es.object.assign',
+  'core-js/modules/es.object.to-string',
+  'core-js/modules/es.promise'
+]
+
 const sharedPlugins = [
+  resolve({
+    extensions: ['.ts', '.tsx', '.mjs', '.js', '.jsx', '.json']
+  }),
+  commonjs(),
   babel({
     extensions: ['.ts', '.tsx', '.mjs', '.js', '.jsx'],
     exclude: 'node_modules/**'
@@ -21,16 +34,7 @@ const sharedPlugins = [
 export default [
   {
     input: 'src/task-queue.ts',
-    plugins: [...sharedPlugins],
-    output: {
-      file: 'dist/task-queue.js',
-      format: 'umd',
-      name: 'TaskQueue',
-      exports: 'named'
-    }
-  },
-  {
-    input: 'src/task-queue.ts',
+    external: externals,
     plugins: [...sharedPlugins],
     output: {
       file: 'dist/common/task-queue.js',
@@ -40,10 +44,21 @@ export default [
   },
   {
     input: 'src/task-queue.ts',
+    external: externals,
     plugins: [...sharedPlugins],
     output: {
       file: 'dist/es/task-queue.js',
       format: 'es'
+    }
+  },
+  {
+    input: 'src/task-queue.ts',
+    plugins: [...sharedPlugins],
+    output: {
+      file: 'dist/task-queue.js',
+      format: 'umd',
+      name: 'TaskQueue',
+      exports: 'named'
     }
   },
   {
